bd5e77c9a0568377cba43de134a3c28fdfd95b44
delwerhossain <delwerhossain006@gmail.com>
2024-06-19 12:55:56 +0100

    UI auto date and theme ID get

--- client/src/app/(page)/confirm-order/page.tsx
+++ client/src/app/(page)/confirm-order/page.tsx
@@ -1,13 +1,37 @@
+"use client";
 import OrderForm from "@/components/orderCompo/order-form/OrderForm";
 import Image from "next/image";
+import { useSearchParams } from "next/navigation";
 
 const page = () => {
+  // get url query form data
+  const searchParams = useSearchParams();
+
+  const themeID = searchParams.get("theme");
+  console.log({ themeID });
+  // Get the current date
+  const date = new Date();
+
+  // Format the current date as dd/mm/yyyy
+  const nowDate = `${date.getDate()}/${
+    date.getMonth() + 1
+  }/${date.getFullYear()}`;
+
+  // Calculate the expected delivery date (current date + 4 days)
+  const expectedDate = new Date(date);
+  expectedDate.setDate(date.getDate() + 4);
+
+  // Format the expected delivery date as dd/mm/yyyy
+  const formattedExpectedDate = `${expectedDate.getDate()}/${
+    expectedDate.getMonth() + 1
+  }/${expectedDate.getFullYear()}`;
+
   return (
     <div className="transition-all duration-1000">
       <section className="py-8 relative">
         <div className="w-full max-w-7xl px-4 md:px-5 lg-6 mx-auto">
           <h2 className="font-manrope font-bold text-4xl leading-10 text-black text-center">
-            Payment Overview 
+            Payment Overview
           </h2>
           <p className="mt-3 font-normal text-lg leading-8 text-gray-500 mb-8 text-center">
             Thanks for making a purchase you can check our order summary frm
@@ -16,25 +40,43 @@ const page = () => {
           <div className="main-box border border-gray-200 rounded-xl pt-6 max-w-xl max-lg:mx-auto lg:max-w-full">
             <div className="flex flex-col lg:flex-row lg:items-center justify-between px-6 pb-6 border-b border-gray-200">
               <div className="data">
-                <p className="font-semibold text-base leading-7 text-black">
+                {/* <p className="font-semibold text-base leading-7 text-black">
                   Order Id:{" "}
                   <span className="text-indigo-600 font-medium">#10234987</span>
-                </p>
-                <p className="font-semibold text-base leading-7 text-black mt-4">
-                  Order Payment :{" "}
-                  <span className="text-gray-400 font-medium">
-                    {" "}
-                    18th march 2021
-                  </span>
-                </p>
+                </p> */}
+                <div className="grid grid-cols-1 justify-center items-center gap-1.5 font-semibold text-base leading-7 text-black mt-4">
+                  <p>Order Date :</p>
+                  <hr />
+                  <p className="text-gray-400 font-medium"> {nowDate}</p>
+                </div>
               </div>
-              <select className="focus:border focus:border-red-400 ring-red-700  rounded-full py-4 px-6 font-semibold  leading-7 text-white  bg-gradient-to-r from-amber-500 to-pink-500 max-lg:mt-5 shadow-sm shadow-transparent  duration-500 hover:bg-gradient-to-r hover:from-amber-600 hover:to-pink-700  transform transition  hover:scale-105 md:hover:scale-110 ">
-                <option  selected >
-                Choose Payment Method ?
-                </option>
-                <option value={'bkash'} className="text-black hover:bg-black hover:text-white">Bkash</option>
-                <option value={'COD'} className="text-black hover:bg-black hover:text-white">Cash on delivery</option>
-              </select>
+
+              <div className="grid grid-cols-1 gap-1 justify-center items-center">
+                <label
+                  className="block font-semibold"
+                  htmlFor="Choose Payment Method ?"
+                >
+                  Choose Payment Method ?
+                </label>
+                <select className="focus:border focus:border-red-400 ring-red-700 rounded-full py-4 px-6 font-semibold leading-7 text-white bg-gradient-to-r from-amber-500 to-pink-500 max-lg:mt-5 shadow-sm shadow-transparent duration-500 hover:bg-gradient-to-r hover:from-amber-600 hover:to-pink-700 transform transition  md:hover:scale-105 lg:hover:scale-110">
+                  {/* <option disabled selected>
+                    Choose Payment Method ?
+                  </option> */}
+                  <option
+                    selected
+                    defaultValue={"Bkash"}
+                    className="text-black hover:bg-black hover:text-white"
+                  >
+                    Bkash
+                  </option>
+                  <option
+                    value={"COD"}
+                    className="text-black hover:bg-black hover:text-white"
+                  >
+                    Cash on delivery
+                  </option>
+                </select>
+              </div>
             </div>
             <div className="w-full px-3 min-[400px]:px-6">
               <div className="flex flex-col lg:flex-row items-center py-6 border-b border-gray-200 gap-6 w-full">
@@ -55,7 +97,7 @@ const page = () => {
                           Premium Personal Website
                         </h2>
                         <p className="font-normal text-lg leading-8 text-gray-500 mb-3 ">
-                          Theme ID: 001
+                          Theme ID: {themeID ? themeID : "loading..."}
                         </p>
                         {/* <div className="flex items-center ">
                           <p className="font-medium text-base leading-7 text-black pr-4 mr-4 border-r border-gray-200">
@@ -67,19 +109,19 @@ const page = () => {
                         </div> */}
                       </div>
                     </div>
-                    <div className="grid grid-cols-5">
-                      <div className="col-span-5 lg:col-span-1 flex items-center max-lg:mt-3">
+                    <div className="grid grid-cols-6  gap-3">
+                      <div className="col-span-5 lg:col-span-2 flex items-center max-lg:mt-3">
                         <div className="flex gap-3 lg:block">
                           <p className="font-medium text-sm leading-7 text-black">
-                            price
+                            Discount Price
                           </p>
-                          <p className="lg:mt-4 font-medium text-sm leading-7 text-indigo-600">
-                            ৳ 2000
-                          </p>
+                          <div className="lg:mt-4 font-medium text-sm leading-7 text-indigo-600">
+                          <s>৳2000</s> ৳1499
+                          </div>
                         </div>
                       </div>
                       <div className="col-span-5 lg:col-span-2 flex items-center max-lg:mt-3 ">
-                        <div className="flex gap-3 lg:block">
+                        <div className="flex gap-3 lg:block ">
                           <p className="font-medium text-sm leading-7 text-black">
                             Status
                           </p>
@@ -91,10 +133,10 @@ const page = () => {
                       <div className="col-span-5 lg:col-span-2 flex items-center max-lg:mt-3">
                         <div className="flex gap-3 lg:block">
                           <p className="font-medium text-sm whitespace-nowrap leading-6 text-black">
-                            Expected Delivery Time
+                            Expected Delivery
                           </p>
                           <p className="font-medium text-base whitespace-nowrap leading-7 lg:mt-3 text-emerald-500">
-                            23rd March 2021
+                            {formattedExpectedDate}
                           </p>
                         </div>
                       </div>
@@ -109,9 +151,9 @@ const page = () => {
             </div>
             <div className="w-full border-t border-gray-200 px-6 flex flex-col lg:flex-row items-center justify-between ">
               <div className="flex flex-col sm:flex-row items-center max-lg:border-b border-gray-200">
-                <button className="flex outline-0 py-6 sm:pr-6  sm:border-r border-gray-200 whitespace-nowrap gap-2 items-center justify-center font-semibold group text-lg text-black bg-white transition-all duration-500 hover:text-indigo-600">
+                <button className="flex outline-0 py-6 sm:pr-6  sm:border-r border-gray-200 whitespace-nowrap gap-2 items-center justify-center font-semibold group text-lg text-black bg-white transition-all duration-500 hover:text-red-600">
                   <svg
-                    className="stroke-black transition-all duration-500 group-hover:stroke-indigo-600"
+                    className="stroke-black transition-all duration-500 group-hover:stroke-red-600"
                     xmlns="http://www.w3.org/2000/svg"
                     width="22"
                     height="22"
@@ -128,8 +170,7 @@ const page = () => {
                   Cancel Order
                 </button>
                 <p className="font-medium text-lg text-gray-900 pl-6 py-3 max-lg:text-center">
-                  Payment Method : {" "}
-                  <span className="text-gray-500">Bkash</span>
+                  Payment Method : <span className="text-gray-500">Bkash</span>
                 </p>
               </div>
               <p className="font-semibold text-lg text-black py-6">
--- client/src/components/orderCompo/order-form/OrderForm.tsx
+++ client/src/components/orderCompo/order-form/OrderForm.tsx
@@ -1,13 +1,60 @@
-const OrderForm = () => {
+"use client";
+import { FC, FormEvent, useState } from "react";
+
+const OrderForm: FC = () => {
+  // state
+  const [orderData, setOrderData] = useState({
+    fullName: "",
+    address: "",
+    email: "",
+    phoneNumber: 0,
+    occupation: "",
+    description: "",
+  });
+
+  const orderSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const { fullName, address, email, phoneNumber, occupation, description } = e.target as typeof e.target & {
+      fullName: { value: string };
+      address: { value: string };
+      email: { value: string };
+      phoneNumber: { value: string };
+      occupation: { value: string };
+      description: { value: string };
+    };
+
+    const updatedOrderData = {
+      fullName: fullName.value,
+      address: address.value,
+      email: email.value,
+      phoneNumber: parseInt(phoneNumber.value),
+      occupation: occupation.value,
+      description: description.value,
+    };
+
+    setOrderData(updatedOrderData);
+
+    // Log the updated order data
+    console.log(updatedOrderData);
+  };
+
+  console.log({ out: orderData });
+
   return (
-    <div className="w-full border rounded-xl   p-4 md:p-8">
-      <form className="flex justify-center items-center justify-items-center flex-wrap -m-3">
+    <div className="w-full border rounded-xl p-4 md:p-8">
+      <form
+        onSubmit={orderSubmit}
+        className="flex justify-center items-center justify-items-center flex-wrap -m-3"
+      >
         <div className="w-full md:w-1/2 p-3">
           <label className="block">
             <input
-              className="  px-4 py-4 w-full text-gray-700 font-semibold tracking-tight placeholder-gray-300 outline-none border border-gray-300 focus:border-indigo-400 rounded-lg transition duration-200"
-              id="contactInput1-1"
+              className="px-4 py-4 w-full text-gray-700 font-semibold tracking-tight placeholder-gray-300 outline-none border border-gray-300 focus:border-indigo-400 rounded-lg transition duration-200"
+              id="fullName"
+              name="fullName"
               type="text"
+              required
               placeholder="Full Name"
             />
           </label>
@@ -16,8 +63,9 @@ const OrderForm = () => {
           <label className="block">
             <input
               className="px-4 py-4 w-full text-gray-700 font-semibold tracking-tight placeholder-gray-300 outline-none border border-gray-300 focus:border-indigo-400 rounded-lg transition duration-200"
-              id="contactInput1-2"
-              type="address"
+              id="address"
+              name="address"
+              type="text"
               placeholder="Address"
             />
           </label>
@@ -26,8 +74,10 @@ const OrderForm = () => {
           <label className="block">
             <input
               className="px-4 py-4 w-full text-gray-700 font-semibold tracking-tight placeholder-gray-300 outline-none border border-gray-300 focus:border-indigo-400 rounded-lg transition duration-200"
-              id="contactInput1-3"
+              id="email"
+              name="email"
               type="email"
+              required
               placeholder="Email Address"
             />
           </label>
@@ -36,33 +86,48 @@ const OrderForm = () => {
           <label className="block">
             <input
               className="px-4 py-4 w-full text-gray-700 font-semibold tracking-tight placeholder-gray-300 outline-none border border-gray-300 focus:border-indigo-400 rounded-lg transition duration-200"
-              id="contactInput1-4"
+              id="phoneNumber"
+              name="phoneNumber"
               type="number"
+              required
               placeholder="Phone Number"
             />
           </label>
         </div>
-       
         <div className="w-full p-3">
-          <select className="focus:border-gray-400 focus:border border border-gray-300 rounded-lg py-4 px-6  text-center font-semibold w-full selection:border-indigo-300 focus:ring-blue-500">
-            <option disabled selected>
+          <select
+            id="occupation"
+            name="occupation"
+            required
+            defaultValue=""
+            className="focus:border-gray-400 focus:border border border-gray-300 rounded-lg py-4 px-6 text-center font-semibold w-full selection:border-indigo-300 focus:ring-blue-500"
+          >
+            <option disabled value="">
               What is your occupation?
             </option>
-            <option>Business Owner</option>
-            <option>Job Holder</option>
-            <option>Freelancer</option>
-            <option>Job Candidate</option>
-            <option>Student</option>
+            <option value="Business Owner">Business Owner</option>
+            <option value="Job Holder">Job Holder</option>
+            <option value="Freelancer">Freelancer</option>
+            <option value="Job Candidate">Job Candidate</option>
+            <option value="Student">Student</option>
           </select>
         </div>
-        
+        <div className="w-full p-3">
+          <textarea
+            id="description"
+            name="description"
+            placeholder="Description of your occupation"
+            className="focus:border-gray-400 focus:border border border-gray-300 rounded-lg py-4 px-6 text-center font-semibold w-full selection:border-indigo-300 focus:ring-blue-500"
+            rows="4"
+          />
+        </div>
         <div className="w-full lg:w-2/5 p-3">
-          <a
-            className="inline-block mb-4 px-4 py-3.5 w-full text-lg text-white text-center font-semibold tracking-tight bg-gradient-to-r from-amber-500 to-pink-600 hover:bg-gradient-to-r hover:from-amber-600 hover:to-pink-700 rounded-full focus:ring-4 focus:ring-pink-300 transform duration-500 transition  hover:scale-105 md:hover:scale-105"
-            href="#"
+          <button
+            className="inline-block mb-4 px-4 py-3.5 w-full text-lg text-white text-center font-semibold tracking-tight bg-gradient-to-r from-amber-500 to-pink-600 hover:bg-gradient-to-r hover:from-amber-600 hover:to-pink-700 rounded-full focus:ring-4 focus:ring-pink-300 transform duration-500 transition hover:scale-105 md:hover:scale-105"
+            type="submit"
           >
-            Place Order
-          </a>
+            Pay Now
+          </button>
           <span className="text-sm text-gray-600 tracking-tight">
             * We never share user details with third parties, period.
           </span>
